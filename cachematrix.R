69a4c6cb6e228f8e2e34c0888d75499cda7f0e9a
Kevin Ostheimer <info@impulsleistung.de>
2014-07-27 16:04:00 +0200

    Stable

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+# By Kevin Ostheimer
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# This function creates a special "matrix" object that can cache its inverse
+makeCacheMatrix <- function(x_mat = matrix()) {
+  m_inverse <- NULL
+  # The constructor x_mat and m_inverse are static
+  set <- function(y_mat) {
+    x_mat <<- y_mat
+    m_inverse <<- NULL
+  }
+  get <- function() x_mat
+  # This function is called from an upper frame
+  setInverse <- function(inverse) m_inverse <<- inverse
+  getInverse <- function() m_inverse
+  # The internal functions are pointed to listvariables
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+# If the inverse has already been calculated
+cacheSolve <- function(mat_norm) {
+  ## Return a matrix that is the inverse of 'mat_norm'
+  m <- mat_norm$getInverse()
+  # If is not empty, the inverse is already calculated...
+  if(!is.null(m)) {
+    message("getting cached data of inverse matrix")
+    return(m)
+  }
+  # Else m is empty, the calculation routine comes here
+  data <- mat_norm$get()
+  m <- solve(data)
+  # Setting the result to the frame of the argument mat_norm
+  mat_norm$setInverse(m)
+  m # Display the result
 }
+
+###############################################################
+## Testprocedure for Programming Assignment 2: Lexical Scoping
+## Feel free to uncomment ;-)
+###############################################################
+# zzz<-matrix(rnorm(n = 9,mean = 10,sd = 2),nrow = 3,ncol = 3)
+# aaa<-makeCacheMatrix(zzz)
+# cacheSolve(aaa)
+# cacheSolve(aaa) # This is the proof
